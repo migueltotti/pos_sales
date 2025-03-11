64bd8b01177b994f775b418ae6bd4349676ad4e6
Miguel Totti <migueltotti2005@gmail.com>
2025-03-11 14:56:23 -0300

    feat(nav): add responsive nav bar and offcanva

--- posSales/src/app/app.component.ts
+++ posSales/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./components/card/card/card.component";
+import { NavBarComponent } from "./components/navBar/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
